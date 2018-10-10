05ae179a84c3226975449f16157267a35edc540a
truongcongbach <truongcongbach1996@gmail.com>
2018-10-10 10:31:33 +0700

    redux example in redux.js.org

--- src/containers/AddTodo.js
+++ src/containers/AddTodo.js
@@ -2,27 +2,47 @@ import React       from 'react'
 import { connect } from 'react-redux'
 import { addTodo } from '../actions'
 
-const AddTodo = ({dispatch}) => {
-    let input
 
-    return (
-        <div>
-            <form
-                onSubmit={e => {
-                    e.preventDefault();
-                    if (!input.value.trim()) {
-                        return
-                    }
-                    dispatch(addTodo(input.value));
-                    input.value = ''
-                }}>
-                <input ref={node => input = node}/>
-                <button type="submit">
-                    Add to do
-                </button>
-            </form>
-        </div>
-    )
+const mapStateToProps = (store, state) => {
+    console.log(store);
+    return {
+        ...state
+    }
+};
+
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        addTodoComponent: (text) => {
+            dispatch(addTodo(text))
+        }
+    }
+};
+
+class AddTodo extends React.Component {
+
+    state = {
+        value: ''
+    };
+
+    onSubmitAddTodo(e) {
+        e.preventDefault();
+        this.props.addTodoComponent(this.state.value);
+        this.setState({value: ''})
+    }
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.onSubmitAddTodo.bind(this)}>
+                    <input name='todo' value={this.state.value} onChange={(e) => this.setState({value: e.target.value})}/>
+                    <button type="submit">
+                        Add to do
+                    </button>
+                </form>
+            </div>
+        )
+    }
 }
 
-export default connect()(AddTodo)
+export default connect(mapStateToProps, mapDispatchToProps)(AddTodo)
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,7 +2,4 @@ import { combineReducers } from 'redux'
 import todos               from './todos'
 import visibilityFilter    from './visibilityFilter'
 
-export default combineReducers({
-    todos,
-    visibilityFilter
-})
+export default combineReducers({todos, visibilityFilter})
